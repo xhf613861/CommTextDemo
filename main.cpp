b4f94099c367a54d975e8d995d8992e44e5558c0
xinhaifeng <haifeng.xin@eeoa.com>
2023-04-07 11:13:32 +0800

    first add file
    Signed-off-by: xinhaifeng <haifeng.xin@eeoa.com>

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+#include "DynamicTest.h"
+#include "MoreEffective31.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+//    testDy();
+    testMoreEffect31();
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
